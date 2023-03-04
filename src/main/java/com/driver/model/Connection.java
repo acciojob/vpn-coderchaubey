770b92646e16f61bbe6231039a458ec9197e8877
coderchaubey <sunnychaubeysm@gmail.com>
2023-03-04 19:21:09 +0530

    Lets Go

--- src/main/java/com/driver/model/Connection.java
+++ src/main/java/com/driver/model/Connection.java
@@ -4,25 +4,21 @@ import javax.persistence.*;
 
 @Entity
 @Table
-public class Country {
+public class Connection {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-
-    @Enumerated(EnumType.STRING)
-    private CountryName countryName;
-
-    private String code;
-
-    @OneToOne
+    @ManyToOne
+    @JoinColumn
     private User user;
 
     @ManyToOne
     @JoinColumn
     private ServiceProvider serviceProvider;
 
-    public Country() {
+    public Connection() {
     }
 
     public int getId() {
@@ -31,22 +27,6 @@ public class Country {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public CountryName getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(CountryName countryName) {
-        this.countryName = countryName;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public User getUser() {
